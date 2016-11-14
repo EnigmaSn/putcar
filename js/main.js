89cbad43d3939f75f9cf5d0b42cf4a80af64f87a
EnigmaSn <enigmasnape@gmail.com>
2016-11-14 17:30:33 +0300

    1

---
+++ js/main.js
@@ -0,0 +1,31 @@
+// объявление переменной
+// поиск по всему DOM-дереву, возврат первого значения
+var menu_button = document.querySelector(".page-header__menu--close");
+var menu = document.querySelector(".page-header__menu--open");
+var header = document.querySelector(".page-header__wrapper");
+
+// отлавливает событие
+menu_button.addEventListener("click", function(event) {
+  if (menu.classList.contains("page-header__menu--open-show")) {
+      // отмена действия по умолчанию
+      event.preventDefault();
+      // вывод в консоль
+      console.log("клик таки");
+      // без точки, Карл, ибо этот метод работает только с классами
+      menu.classList.remove("page-header__menu--open-show");
+  }
+  else {
+      event.preventDefault();
+      menu.classList.add("page-header__menu--open-show");
+  }
+});
+
+// фон шапки
+menu.addEventListener("click", function(event) {
+  if (header.classList.contains("page-header__wrapper-show-menu")) {
+      header.classList.remove("page-header__wrapper-show-menu");
+  }
+  else {
+      header.classList.add("page-header__wrapper-show-menu");
+  }
+});
