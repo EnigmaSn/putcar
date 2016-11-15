604245fa19f32cc3027a2606311f9c0db633128a
EnigmaSn <enigmasnape@gmail.com>
2016-11-16 01:02:05 +0300

    1

--- js/main.js
+++ js/main.js
@@ -1,35 +1,32 @@
 // объявление переменной
 // поиск по всему DOM-дереву, возврат первого значения
-var menu_button = document.querySelector(".page-header__menu--close");
-var menu = document.querySelector(".page-header__menu--open");
-var header = document.querySelector(".page-header__wrapper");
+var menu_button = $(".page-header__menu--close");
+var menu = $(".page-header__menu--open");
+var header = $(".page-header__wrapper");
 
 // отлавливает событие
-menu_button.addEventListener("click", function(event) {
-  if (menu.classList.contains("page-header__menu--open-show")) {
-      // отмена действия по умолчанию
-      event.preventDefault();
+menu_button.click(function(event) {
+  if (menu.hasClass("page-header__menu--open-show")) {
       // вывод в консоль
       console.log("клик таки");
       // без точки, Карл, ибо этот метод работает только с классами
       // menu.classList.remove("page-header__menu--open-show");
+  } else {
+      menu.addClass("page-header__menu--open-show");
   }
-  else {
-      event.preventDefault();
-      menu.classList.add("page-header__menu--open-show");
-  }
+  return false;
 });
 
 // фон шапки
-menu.addEventListener("click", function(event) {
-  header.classList.add("page-header__wrapper-show-menu");
+menu_button.click(function(event) {
+  header.addClass("page-header__wrapper-show-menu");
 });
 
 
-var menu_close = document.querySelector(".page-header__menu-close");
-    menu_close.addEventListener("click", function(event) {
-        event.preventDefault();
-        menu.classList.remove("page-header__menu--open-show");
+var menu_close = $(".page-header__menu-close");
+    menu_close.click(function(event) {
+        menu.removeClass("page-header__menu--open-show");
+        return false;
     });
 
 
